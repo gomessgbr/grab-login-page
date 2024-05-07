52c20cdf79ce85b96fb670ebee56edf602c0eded
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-05-07 12:15:11 -0300

    feat: change colors of border when invalid value is submitted

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 let formElement = document.getElementById("signInForm");
+const inputEmail = document.getElementById('signEmail');
+const labelEmail = document.getElementById('labelSignEmail');
+const iconEmail = document.getElementById("imageSignEmail");
+const inputPassword = document.getElementById("signPswd");
+const labelPassword = document.getElementById('labelSignPassword');
+const iconPassword = document.getElementById("imageSignPassword");
+
 
 formElement.addEventListener("submit", (event) => {
   event.preventDefault();
   // format a got data from form
   new FormData(formElement);
 });
+
+inputEmail.addEventListener('invalid',() => {
+  labelEmail.style.borderColor = '#FF5757';
+  iconEmail.style.fill = '#FF5757 !important';
+})
+
+inputPassword.addEventListener('invalid',() => {
+  labelPassword.style.borderColor = '#FF5757';
+  iconPassword.style.fill = '#FF5757 !important';
+})
+
 
 formElement.addEventListener("formdata", (e) => {
   // Get the form data from the event object
